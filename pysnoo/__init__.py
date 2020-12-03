b826c8319fd6de2cc95078f5893e6566ccd61d34
Martin Riedel <web@riedel-privat.de>
2020-12-02 21:42:42 -0500

    chore: initial project setup

---
+++ pysnoo/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""Python Module to use with SNOO Smart Sleeper Bassinet
+
+Contains classes to authenticate against the Happiest Baby API and query various
+entities and expose their functionality.
+"""
---
+++ pysnoo/simple.py
@@ -0,0 +1,8 @@
+"""Simple Class
+
+"""
+
+
+def add(val_a, val_b):
+    """Add Numbers"""
+    return val_a + val_b
